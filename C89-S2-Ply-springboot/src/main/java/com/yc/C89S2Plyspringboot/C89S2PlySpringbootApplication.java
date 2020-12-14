8c53abfb1fe1334a262363853b585ca50da8dc2e
root <root>
2020-12-14 17:37:19 +0800

    WebSocket在线聊天

--- C89-S2-Ply-springboot/src/main/java/com/yc/C89S2Plyspringboot/C89S2PlySpringbootApplication.java
+++ C89-S2-Ply-springboot/src/main/java/com/yc/C89S2Plyspringboot/C89S2PlySpringbootApplication.java
@@ -2,6 +2,8 @@ package com.yc.C89S2Plyspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class C89S2PlySpringbootApplication {
@@ -9,5 +11,14 @@ public class C89S2PlySpringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(C89S2PlySpringbootApplication.class, args);
 	}
+	
+	/**
+	 * WebSocket 服务器对象
+	 * @return
+	 */
+	@Bean 
+	public ServerEndpointExporter serverEndpointExporter() { 
+		return new ServerEndpointExporter(); 
+	} 
 	
 }
---
+++ C89-S2-Ply-springboot/src/main/java/com/yc/C89S2Plyspringboot/biz/MyWebSocket.java
@@ -0,0 +1,65 @@
+package com.yc.C89S2Plyspringboot.biz;
+
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Map.Entry;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.stereotype.Component;
+
+// 定义服务器地址
+@ServerEndpoint(value = "/websocket/{id}")
+@Component
+public class MyWebSocket {
+	// Hashtable 用来存放每个客户端对应的 id : Session 对象。
+	private static Hashtable<String,Session> webSocketMap = new Hashtable<>();
+	
+	/**
+	 * Hashtable, HashMap 的区别
+	 * 1. Hashtable 线程安全   （访问效率低， 安全性高）
+	 * 2. Hashtable 不允许null作为key值
+	 */
+
+	// 连接建立成功调用的方法    @PathParam 地址参数  /websocket/{id} ==》 id
+	@OnOpen
+	public void onOpen(@PathParam("id")String id, Session session) {
+		// 将id保存到会话
+		session.getUserProperties().put("myid", id);
+		webSocketMap.put(id, session);
+	}
+
+	// 连接关闭调用的方法
+	@OnClose
+	public void onClose(Session session) {
+		// 删除会话
+		String id = (String) session.getUserProperties().get("myid");
+		webSocketMap.remove(id);
+	}
+
+	// 收到客户端消息后调用的方法
+	@OnMessage
+	/**
+	 * 
+	 * @param message
+	 * @param session 发送者的session
+	 * @throws IOException
+	 */
+	public void onMessage(String message, Session session) throws IOException {
+		// 消息的格式：如何知道该消息发给谁？  id:msg
+		String[] items = message.split(":");
+		String youid = items[0];
+		String msg = items[1];
+		String myid = (String) session.getUserProperties().get("myid");
+		// 查找接受者的Session
+		Session recvSession = webSocketMap.get(youid);
+		// 发送消息
+		recvSession.getBasicRemote().sendText(myid + ":" + msg);
+	}
+}
+
