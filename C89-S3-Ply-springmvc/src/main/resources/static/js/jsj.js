ab5ee75ccfa5623683ee6fda13a940bf31f22385
lls <306529917@qq.com>
2021-01-19 15:23:53 +0800

    将分类列表 vue 组件，移到jsj.js中

--- C89-S3-Ply-springmvc/src/main/resources/static/js/jsj.js
+++ C89-S3-Ply-springmvc/src/main/resources/static/js/jsj.js
@@ -125,3 +125,24 @@ Vue.component(
 
 	}
 )
+
+Vue.component(
+	'cate',
+	{
+		data : function(){
+			return { calist : {} };
+		},
+		created(){
+			axios.get("selectAllCate.do").then(res=>{
+				this.calist=res.data;
+			})
+		},
+		template : `<div class="side_block">
+					<div class="title">书籍分类</div>
+					<div class="separate"></div>
+					<div id="booktype_sidebar_div">
+						<a v-for="c in calist" :href="'category.html?'+c.id" category_key="1000">{{c.name}}</a> 
+					</div>
+				</div>`
+	}
+)
