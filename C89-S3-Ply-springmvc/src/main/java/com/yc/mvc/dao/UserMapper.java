31377cf6e4c5668f61e85e406dde1adf03fa1530
basheng111 <850074549@qq.com>
2021-01-21 00:23:16 +0800

    收货信息

--- C89-S3-Ply-springmvc/src/main/java/com/yc/mvc/dao/UserMapper.java
+++ C89-S3-Ply-springmvc/src/main/java/com/yc/mvc/dao/UserMapper.java
@@ -12,7 +12,7 @@ import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
-import com.yc.mvc.po.JsjFans;
+import com.yc.mvc.po.JsjDict;
 import com.yc.mvc.po.JsjUser;
 
 public interface UserMapper {
@@ -40,38 +40,31 @@ public interface UserMapper {
 	List<JsjUser> selectNewRegUser();
 
 	@Select("select * from jsj_user where id=#{id}")
-	@Results(id="rmuser", value = { @Result(column = "school", property = "schoolObj", 
-	one = @One(select = "com.yc.mvc.dao.SchoolMapper.selectById")),
-			@Result(column = "id",property = "fans",
-			many = @Many(select = "selectFans")),
-			@Result(column = "id",property = "guanzhu",
-			many = @Many(select = "selectGuanzhu"))})
+	@Results(id = "rmuser", value = {
+			@Result(column = "school", property = "schoolObj", one = @One(select = "com.yc.mvc.dao.SchoolMapper.selectById")),
+			@Result(column = "id", property = "fans", many = @Many(select = "selectFans")),
+			@Result(column = "id", property = "guanzhu", many = @Many(select = "selectGuanzhu")) })
 	public JsjUser selectById(int id);
-	
 
 	@Update("update jsj_user set collect_type=#{collectType},"
-			+ "collect_account=#{collectAccount},collect_name=#{collectName} "
-			+ "where id=#{id}")
+			+ "collect_account=#{collectAccount},collect_name=#{collectName} " + "where id=#{id}")
 	void addcollect(JsjUser user);
 
-
 	@Select("select * from jsj_fans a left JOIN jsj_user b on a.fid = b.id where uid = #{uid}")
-	@Results(id = "rmschool",value = { @Result(column = "school", property = "schoolObj", 
-	one = @One(select = "com.yc.mvc.dao.SchoolMapper.selectById"))})
+	@Results(id = "rmschool", value = {
+			@Result(column = "school", property = "schoolObj", one = @One(select = "com.yc.mvc.dao.SchoolMapper.selectById")) })
 	public List<JsjUser> selectFans(int uid);
-	
+
 	@Select("select * from jsj_fans a left JOIN jsj_user b on a.uid = b.id where fid = #{fid}")
 	@ResultMap("rmschool")
 	public List<JsjUser> selectGuanzhu(int fid);
 
 	@Update("update jsj_user set sign=#{sign} where id=#{id}")
-	void updateJsjUserSign(String sign,int id) ;
-	
+	void updateJsjUserSign(String sign, int id);
+
 	@Update("update jsj_user set pwd=#{pwd} where id=#{id}")
 	void updatePwd(String pwd, Integer id);
 
-	
-	
 //	@Select("select * from jsj_fans GROUP BY uid ORDER BY count(*) desc LIMIT 0,24")
 //	@Results(value = { @Result(column = "uid", property = "user", 
 //	one = @One(select = "selectById"))})
@@ -79,4 +72,10 @@ public interface UserMapper {
 			+ " GROUP BY a.uid ORDER BY count(*) desc LIMIT 0,24 ")
 	@ResultMap("rmschool")
 	public List<JsjUser> selectMostGuanZhu();
+
+	@Update("update jsj_user set addr_name=#{addrName},addr_phone=#{addrPhone},addr_post=#{addrPost},addr_province=#{addrProvince},addr_desc=#{addrDesc} where account=#{account}")
+	void updateaddr(JsjUser user);
+
+	@Select("select * from jsj_dict")
+	List<JsjDict> getAllProvince();
 }
--- C89-S3-Ply-springmvc/src/main/java/com/yc/mvc/web/UserAction.java
+++ C89-S3-Ply-springmvc/src/main/java/com/yc/mvc/web/UserAction.java
@@ -2,9 +2,7 @@ package com.yc.mvc.web;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import javax.annotation.Resource;
@@ -23,6 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.yc.mvc.biz.BizException;
 import com.yc.mvc.biz.UserBiz;
 import com.yc.mvc.dao.UserMapper;
+import com.yc.mvc.po.JsjDict;
 import com.yc.mvc.po.JsjUser;
 import com.yc.mvc.web.po.Result;
 
@@ -31,10 +30,10 @@ public class UserAction {
 
 	@Resource
 	private UserBiz ubiz;
-	
+
 	@Resource
 	private UserMapper um;
-	
+
 	@PostMapping("login.do")
 	public Result login(JsjUser user, HttpSession session) throws BizException {
 		JsjUser dbuser;
@@ -47,38 +46,29 @@ public class UserAction {
 			return Result.failure(e.getMessage(), null);
 		}
 	}
-	
+
 	@RequestMapping("getLoginedUser.do")
 	public Result getLoginedUser(@SessionAttribute JsjUser loginedUser) {
 		return Result.success("获取用户对象成功", loginedUser);
 	}
-	
+
 	/**
-	 * 	注册用户， SpringMVC 验证功能
-	 * 	第一步：pom依赖：添加验证依赖
-	 *  <dependency>
-			<groupId>javax.validation</groupId>
-			<artifactId>validation-api</artifactId>
-		</dependency>
-		<dependency>
-			<groupId>org.hibernate.validator</groupId>
-			<artifactId>hibernate-validator</artifactId>
-		</dependency>
-		规范数据验证：JSR303规范
-		
-		第二步：在实体类属性上添加验证注解
-		第三步： 在控制器方法中定义 错误对象
-			1. 在被验证的对象前添加 Valid 注解
-			2.在被验证的对象后添加 Errors 对象参数（必须是紧跟被验证的对象参数后）
-		
+	 * 注册用户， SpringMVC 验证功能 第一步：pom依赖：添加验证依赖 <dependency>
+	 * <groupId>javax.validation</groupId> <artifactId>validation-api</artifactId>
+	 * </dependency> <dependency> <groupId>org.hibernate.validator</groupId>
+	 * <artifactId>hibernate-validator</artifactId> </dependency> 规范数据验证：JSR303规范
+	 * 
+	 * 第二步：在实体类属性上添加验证注解 第三步： 在控制器方法中定义 错误对象 1. 在被验证的对象前添加 Valid 注解 2.在被验证的对象后添加
+	 * Errors 对象参数（必须是紧跟被验证的对象参数后）
+	 * 
 	 * @param user
 	 * @return
 	 */
 	@RequestMapping("regist.do")
 	public Result regist(@Valid JsjUser user, Errors errors) {
 		// 判断是否出现验证错误
-		if(errors.hasFieldErrors("phone")||errors.hasFieldErrors("name")||errors.hasFieldErrors("account")
-				||errors.hasFieldErrors("pwd")||errors.hasFieldErrors("email")||errors.hasFieldErrors("gender")) {
+		if (errors.hasFieldErrors("phone") || errors.hasFieldErrors("name") || errors.hasFieldErrors("account")
+				|| errors.hasFieldErrors("pwd") || errors.hasFieldErrors("email") || errors.hasFieldErrors("gender")) {
 			return Result.failure("字段验证错误！", errors.getAllErrors());
 		}
 		// TODO 业务层代码， 未完待续
@@ -86,108 +76,130 @@ public class UserAction {
 			ubiz.register(user);
 			return Result.success("注册成功！", null);
 		} catch (Exception e) {
-			//e.printStackTrace();
+			// e.printStackTrace();
 			errors.rejectValue("account", "NotOne", e.getMessage());
 			return Result.failure("字段验证错误", errors.getAllErrors());
 		}
-		
+
 	}
-	
+
 	/**
-	 * 	二阶段 Servlet 如何接收上传的文件
-	 * 	@MutilPartConfig
-	 * 	public class XXXServlet extends HttpServlet{
-	 * 		public void doPost(req,resp){
-	 * 			Part part = req.getPart("headImgFile");
-	 * 			part.write("文件路径");
-	 * 		}
-	 * 	}
+	 * 二阶段 Servlet 如何接收上传的文件
+	 * 
+	 * @MutilPartConfig public class XXXServlet extends HttpServlet{ public void
+	 *                  doPost(req,resp){ Part part = req.getPart("headImgFile");
+	 *                  part.write("文件路径"); } }
 	 * 
 	 * 
 	 * @return
-	 * @throws IOException 
-	 * @throws IllegalStateException 
+	 * @throws IOException
+	 * @throws IllegalStateException
 	 */
 	@PostMapping("upload.do")
 	public Result upload(@RequestParam("headImgFile") MultipartFile headImgFile,
-			@SessionAttribute JsjUser loginedUser){
+			@SessionAttribute JsjUser loginedUser) {
 		// 1. 保存文件 （保存到磁盘上）
-		String newfile = UUID.randomUUID().toString();//   1  时间戳， 2 随机数，  3，UUID
+		String newfile = UUID.randomUUID().toString();// 1 时间戳， 2 随机数， 3，UUID
 		String oldfile = headImgFile.getOriginalFilename();
 		// 获取原文件的后缀名
 		String suffix = oldfile.substring(oldfile.lastIndexOf("."));
 		System.out.println(suffix);
-		if(suffix.equalsIgnoreCase(".png")||suffix.equalsIgnoreCase(".jpg")||suffix.equalsIgnoreCase(".gif")||suffix.equalsIgnoreCase(".jpeg")
-				||suffix.equalsIgnoreCase(".pdf")) {
+		if (suffix.equalsIgnoreCase(".png") || suffix.equalsIgnoreCase(".jpg") || suffix.equalsIgnoreCase(".gif")
+				|| suffix.equalsIgnoreCase(".jpeg") || suffix.equalsIgnoreCase(".pdf")) {
 			newfile += suffix;
 			try {
 				headImgFile.transferTo(new File("d:/bean/upload_head", newfile));
 			} catch (IllegalStateException | IOException e) {
 				e.printStackTrace();
-				return Result.failure(0,"文件上传失败！", null);
+				return Result.failure(0, "文件上传失败！", null);
 			}
-			// 2. 返回图片的web路径 
+			// 2. 返回图片的web路径
 			String webpath = "upload_head/" + newfile;
 			loginedUser.setHeadImg(webpath);
 			ubiz.updateHeadImg(loginedUser);
-			return Result.success(1,"文件上传成功！", webpath);
-		}else {
-			return Result.failure(0,"文件上传失败，文件不是图片！", suffix);
+			return Result.success(1, "文件上传成功！", webpath);
+		} else {
+			return Result.failure(0, "文件上传失败，文件不是图片！", suffix);
 		}
-		
+
 	}
 
 	@RequestMapping("getlastTimeUsers.do")
 	public List<JsjUser> getlastTimeUsers() {
 		return um.getlastTimeUsers();
 	}
-	
+
 	@GetMapping("NewReg.do")
 	public List<JsjUser> selectNewReg() {
 		List<JsjUser> newReg;
-		newReg =um.selectNewRegUser();
+		newReg = um.selectNewRegUser();
 		return newReg;
 	}
 
-	
-
 	@PostMapping("addCollect.do")
-	public Result addCollect(@Valid JsjUser user,Errors errors,@SessionAttribute JsjUser loginedUser) {
-		if(errors.hasFieldErrors("collectType")||errors.hasFieldErrors("collectAccount")||errors.hasFieldErrors("collectName")) {
+	public Result addCollect(@Valid JsjUser user, Errors errors, @SessionAttribute JsjUser loginedUser) {
+		if (errors.hasFieldErrors("collectType") || errors.hasFieldErrors("collectAccount")
+				|| errors.hasFieldErrors("collectName")) {
 			return Result.failure("字段验证错误！", errors.getAllErrors());
 		}
 		user.setId(loginedUser.getId());
 		um.addcollect(user);
 		System.out.println(user.getCollectAccount());
-		return Result.success("提交收款账号成功",null);
+		return Result.success("提交收款账号成功", null);
 	}
 
 	@RequestMapping("selectById")
 	public JsjUser selectById(int id) {
-		return  um.selectById(id);		
+		return um.selectById(id);
 	}
 
 	@PostMapping("sign.do")
-	public Result updateSign(String sign,int id) {
-		um.updateJsjUserSign(sign,id);
+	public Result updateSign(String sign, int id) {
+		um.updateJsjUserSign(sign, id);
 		return Result.success("修改成功", null);
-	}
-	
-	@PostMapping("updatePwd.do")
-	public Result updatePwd(String Npwd,String repwd,@SessionAttribute JsjUser loginedUser) {
-		if(Npwd.equals(repwd)) {
-		um.updatePwd(Npwd,loginedUser.getId());
-		return Result.success("修改成功", null);
-		}
-		else {
-		return Result.success("重复密码不一致", null);	
-		}
 	}
 
+	@PostMapping("updatePwd.do")
+	public Result updatePwd(String Npwd, String repwd, @SessionAttribute JsjUser loginedUser) {
+		if (Npwd.equals(repwd)) {
+			um.updatePwd(Npwd, loginedUser.getId());
+			return Result.success("修改成功", null);
+		} else {
+			return Result.success("重复密码不一致", null);
+		}
+	}
 
 	@RequestMapping("selectMostGuanZhu")
-	public List<JsjUser> selectMostGuanZhu(){
+	public List<JsjUser> selectMostGuanZhu() {
 		return um.selectMostGuanZhu();
+	}
+
+	@GetMapping("getaddr.do")
+	public JsjUser getaddr(@SessionAttribute JsjUser loginedUser) {
+
+		return um.selectByAccount(loginedUser.getAccount());
+	}
+
+	@PostMapping("updateaddr.do")
+	public Result updateaddr(@Valid JsjUser user, Errors errors, @SessionAttribute JsjUser loginedUser) {
+		if (errors.hasFieldErrors("addrName") || errors.hasFieldErrors("addrPhone")
+				|| errors.hasFieldErrors("addrPost")) {
+			return Result.failure("字段验证错误！", errors.getAllErrors());
+		}
+
+		try {
+			user.setAccount(loginedUser.getAccount());
+			um.updateaddr(user);
+			return Result.success(1, "修改收货信息成功！", null);
+		} catch (Exception e) {
+			// e.printStackTrace();
+			return Result.failure(0, "字段验证错误！", errors.getAllErrors());
+		}
+	}
+
+	@GetMapping("getAllProvince.do")
+	public List<JsjDict> getAllProvince() {
+		return um.getAllProvince();
 	}
 
 }
