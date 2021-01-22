6c48e93cbc910bdbfe4f57a26401a5abe5b3af6e
hahas <hahas@LAPTOP-TB2DCIL3>
2021-01-22 16:51:37 +0800

    后台管理的页面以及管理员数据库

---
+++ C89-S3-Ply-springmvc/src/main/resources/static/back/jsj_admin.sql
@@ -0,0 +1,32 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50018
+Source Host           : localhost:3306
+Source Database       : mybaties
+
+Target Server Type    : MYSQL
+Target Server Version : 50018
+File Encoding         : 65001
+
+Date: 2021-01-22 16:42:56
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for jsj_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `jsj_admin`;
+CREATE TABLE `jsj_admin` (
+  `id` int(11) NOT NULL auto_increment,
+  `account` varchar(255) default NULL,
+  `pwd` varchar(255) default NULL,
+  `status` int(11) default NULL COMMENT '0是冻结，1是未冻结',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of jsj_admin
+-- ----------------------------
