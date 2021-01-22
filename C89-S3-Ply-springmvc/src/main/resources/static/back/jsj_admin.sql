3f7b843f07555377e52612daf4b6b49b2a5a476f
hahas <hahas@LAPTOP-TB2DCIL3>
2021-01-22 16:58:32 +0800

    管理员表增加了电话和邮箱字段

--- C89-S3-Ply-springmvc/src/main/resources/static/back/jsj_admin.sql
+++ C89-S3-Ply-springmvc/src/main/resources/static/back/jsj_admin.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50018
 File Encoding         : 65001
 
-Date: 2021-01-22 16:42:56
+Date: 2021-01-22 16:57:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,6 +23,8 @@ CREATE TABLE `jsj_admin` (
   `id` int(11) NOT NULL auto_increment,
   `account` varchar(255) default NULL,
   `pwd` varchar(255) default NULL,
+  `phone` varchar(255) default NULL,
+  `email` varchar(255) default NULL,
   `status` int(11) default NULL COMMENT '0是冻结，1是未冻结',
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
