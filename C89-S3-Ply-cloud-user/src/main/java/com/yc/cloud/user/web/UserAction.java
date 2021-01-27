1ab2690f8a9b0c1ef699ef4925559c10700120a1
lls <306529917@qq.com>
2021-01-27 17:01:06 +0800

    zuul 简单路由配置

--- C89-S3-Ply-cloud-user/src/main/java/com/yc/cloud/user/web/UserAction.java
+++ C89-S3-Ply-cloud-user/src/main/java/com/yc/cloud/user/web/UserAction.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.yc.cloud.user.bean.Numbers;
 
 @RestController
