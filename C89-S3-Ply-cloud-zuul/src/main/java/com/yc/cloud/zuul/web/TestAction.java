cc61b445375b62ca95fea1affe6f2f5a70c638f9
lls <306529917@qq.com>
2021-01-29 14:44:28 +0800

    生成 Blog 项目的实体类 + MyBatis 接口

--- C89-S3-Ply-cloud-zuul/src/main/java/com/yc/cloud/zuul/web/TestAction.java
+++ C89-S3-Ply-cloud-zuul/src/main/java/com/yc/cloud/zuul/web/TestAction.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestAction {
 	
-	@GetMapping("hello")
+	@GetMapping("hello/test")
 	public String hello() {
 		return "hello";
 	}
