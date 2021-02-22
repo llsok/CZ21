10498b6718e6c2ad4c748708f3a10e617e3c3990
lls <306529917@qq.com>
2021-02-22 13:52:58 +0800

    配置中心+服务路由

---
+++ C89-S3-Ply-sq/C89-S3-Ply-sq-zuul/src/main/java/com/yc/sq/ZuulApp.java
@@ -0,0 +1,15 @@
+package com.yc.sq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApp.class, args);
+	}
+
+}
